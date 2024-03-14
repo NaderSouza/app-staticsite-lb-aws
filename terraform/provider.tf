a1dcf7c9af6479cd4335065a8868f3229358f60c
NaderSouza <naderhadadsouza@gmail.com>
2024-03-14 02:13:24 -0300

    fix: test terraforrm

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -10,17 +10,18 @@ terraform {
     }
   }
 
+
   backend "s3" {
-    bucket         = "tf-site-ec2-elb-state-v1"
+    bucket         = "tf-site-ec2-elb-state-v2"
     key            = "terraform.tfstate"
-    dynamodb_table = "tf-site-ec2-elb-state-v1"
+    dynamodb_table = "tf-site-ec2-elb-state-v2"
     region         = "us-east-1"
   }
 
 }
 
 provider "aws" {
-  region                   = "us-east-1"
+  region = "us-east-1"
   # shared_config_files      = ["~/.aws/config"]
   # shared_credentials_files = ["~/.aws/credentials"]
   # profile                  = "fiap"
